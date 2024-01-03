473a1431d0675810f0cb29b82ad27bc08b1ae1bc
WIlliams <akanniwilliams@gmail.com>
2024-01-04 00:38:51 +0100

    Disable lint

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-const-assign */
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
