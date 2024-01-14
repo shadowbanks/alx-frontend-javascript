2b2c8d720d8f7f8b75e29831de56fd215c81a0f2
WIlliams <akanniwilliams@gmail.com>
2024-01-15 00:13:18 +0100

    Twick

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,7 @@
 export const weakMap = new WeakMap();
 export function queryAPI(endPoint) {
   const count = weakMap.get(endPoint) || 0;
-  if (count >= 5) throw new Error('Endpoint load is high');
-
   weakMap.set(endPoint, count + 1);
-
-  return count + 1;
+  if (weakMap.get(endPoint) >= 5) throw new Error('Endpoint load is high');
+  return count;
 }
