664e885e0c082482de3a68f548477d94446d51fa
WIlliams <akanniwilliams@gmail.com>
2024-01-14 23:29:28 +0100

    Twick solution

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,11 @@
 export default function cleanSet(set, startString) {
   let returnOutput = '';
+  const strLen = startString.length;
   let check = false;
+  if (!startString || !strLen) return '';
+
   for (const item of set) {
-    const strLen = startString.length;
-    if (startString === item.slice(0, strLen) && startString) {
+    if (startString === item.slice(0, strLen)) {
       if (check) {
         returnOutput += '-';
       } else check = true;
